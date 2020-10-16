03a8af0356a116a0e6ce287ca3c669c801d2b0c3
hymntaha <tahauygun@gmail.com>
2020-10-16 12:01:26 -0700

    testing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -7,18 +7,21 @@ router.post(
   "/api/users/signup",
   [
     body("email").isEmail().withMessage("Email must be valid"),
-    password("password")
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
     const { email, password } = req.body;
 
-    if (!email || typeof email !== "string") {
-      res.status(400).send("Provide a valid email");
-    }
+    console.log("Creating a user...");
 
+    res.send({});
     // new user
   }
 );
