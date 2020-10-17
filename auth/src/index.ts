7fa043f3a71dad522a535c9cc8e37a1185ab5c67
hymntaha <tahauygun@gmail.com>
2020-10-17 11:58:35 -0700

    quick fix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -25,18 +25,18 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
-  try{
+  try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-  } catch (err){
-    console.error(err)
+  } catch (err) {
+    console.error(err);
   }
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!");
   });
 };
 
-
+start();
