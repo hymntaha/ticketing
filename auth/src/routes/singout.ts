78588bfb99ede1b433fc5d2319a7732a0b2ab10d
hymntaha <tahauygun@gmail.com>
2020-10-19 21:55:10 -0700

    signout

--- auth/src/routes/singout.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/api/users/signout', (resq, res) =>{
-    res.send('Hi there!');
-})
-
-export {router as signoutRouter}
