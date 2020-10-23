881889220533d29bc2f5dafd22b0d6bf71f1d81b
hymntaha <tahauygun@gmail.com>
2020-10-22 23:10:20 -0700

    specifying host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,27 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
-  return response.data;
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // request should be made to http://
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
 };
 
 export default LandingPage;
