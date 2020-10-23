bd501ed039ee63fbd7c6c524c8387abb783ea5bc
hymntaha <tahauygun@gmail.com>
2020-10-22 22:24:27 -0700

    fetching data during ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  return <h1>Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser");
+  return response.data;
+};
+
+export default LandingPage;
