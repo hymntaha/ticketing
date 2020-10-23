3c1582a710c3dae282220bdc61931a23b841d7ee
hymntaha <tahauygun@gmail.com>
2020-10-22 23:12:40 -0700

    passing through cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,16 +5,14 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server!
     // request should be made to http://
     const { data } = await axios.get(
       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
       {
-        headers: {
-          Host: "ticketing.dev",
-        },
+        headers: req.headers
       }
     );
     return data;
