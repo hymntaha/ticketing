18b6f660a1eb3e7a2590e63678476bfc410c85e6
hymntaha <tahauygun@gmail.com>
2020-10-24 23:53:36 -0700

    creating the router

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -3,10 +3,12 @@ import { app } from "../../app";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
-  expect(response.status).not().toEqual(404);
+  expect(response.status).not.toEqual(404);
 });
 
-it("can only be accessed if the user is signed in", async () => {});
+it("can only be accessed if the user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
 
 it("returns an error if an invalid title is provided", async () => {});
 
