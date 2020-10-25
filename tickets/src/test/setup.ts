bf5124f4c5591a77adfcb3e487b281cf41c86e59
hymntaha <tahauygun@gmail.com>
2020-10-25 00:43:55 -0700

    building a session

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,6 +2,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   namespace NodeJS {
@@ -40,9 +41,19 @@ afterAll(async () => {
 
 global.signin = async () => {
   // Build a JWT payload. {id, email}
+  const payload = {
+    id: "dfsdsgsgsdg",
+    email: "test@test.com",
+  };
   // Create JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
   // Build session Object. {jwt: MY_JWT}
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
   // Take JSON and encode  it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
   // return a string thats the cookie with the encoded data
+  return `express:sess=${base64}`;
 };
