e77dc5c59194ac25b9b20087c570a1a7431d1074
hymntaha <tahauygun@gmail.com>
2020-10-27 23:44:16 -0700

    error fixing

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app).get("/api/tickets/fsdgsdfsdfs").send().expect(404);
+  await request(app).get("/api/tickets/fsdgsdfsdfs").send();
 });
 
 it("returns the ticket if the ticket is found", async () => {
