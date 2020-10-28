e17641a2d7b46261016ad00f7931092a9e414d3d
hymntaha <tahauygun@gmail.com>
2020-10-27 22:55:02 -0700

    validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
-import {requireAuth} from '@sgtickets/common';
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@sgtickets/common";
 
 const router = express.Router();
 
-router.post("/api/tickets", (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
-export {router as createTicketRouter};
+export { router as createTicketRouter };
