3591a5e7b61d7e5bd729766d322a018018183c40
hymntaha <tahauygun@gmail.com>
2020-10-29 23:30:28 -0700

    manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,12 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group"
+    "orders-service-queue-group",
+    options
   );
 
   subscription.on("message", (msg: Message) => {
