d3a1c9526ef1dd6f676558e84a27a58ac0f74fa3
hymntaha <tahauygun@gmail.com>
2020-10-30 22:38:43 -0700

    refactoring 1

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,12 @@
+import {Message} from "node-nats-streaming";
+
+export class TicketCreatedListener extends Listener {
+    subject = "ticket:created";
+    queueGroupName = "payments-service";
+
+    onMessage(data: any, msg: Message) {
+        console.log("event data!", data);
+
+        msg.ack();
+    }
+}
